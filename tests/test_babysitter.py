872bae42a83fc77f45d280fb8e57ea9462de5b57
James Prior <jep200404@columbus.rr.com>
2017-11-02 20:42:46 -0400

    tests/test_babysitter.py: Fixed bug in test.

--- tests/test_babysitter.py
+++ tests/test_babysitter.py
@@ -7,6 +7,6 @@ class TestBabySitter:
 
     def test_babysitter_starttime_ontime(self):
         start_time = datetime.time(17,0)
-        babysitter = babysitter.BabySitter(start_time)
+        doubtfire = babysitter.BabySitter(start_time)
 
-        assert babysitter.start_time == start_time
+        assert doubtfire.start_time == start_time
