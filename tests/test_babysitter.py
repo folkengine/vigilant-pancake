4eb2a7a7df9ce536d33092c5ced39afb10d7e860
Christoph <ignatz@gmail.com>
2017-11-02 20:30:48 -0400

    Created start time failing test

--- tests/test_babysitter.py
+++ tests/test_babysitter.py
@@ -1,8 +1,11 @@
 import pytest
+import datetime
 
 
 class TestBabySitter:
 
-    def test_baby_sitter(self):
-        assert True == False
+    def test_babysitter_starttime_ontime(self):
+        start_time = datetime.time(17,0)
+        babysitter = babysitter.BabySitter(start_time)
 
+        assert babysitter.start_time == start_time
