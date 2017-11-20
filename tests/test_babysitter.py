7729e97045b745393908c5bbc8605811d59db07b
Christoph <ignatz@gmail.com>
2017-11-20 12:38:28 -0500

    Added parameterized start times

--- tests/test_babysitter.py
+++ tests/test_babysitter.py
@@ -8,10 +8,15 @@ class TestBabySitter:
 
     billable_start_time = datetime.time(17, 0)
 
-    def test_babysitter_starttime_ontime(self):
-        doubtfire = babysitter.BabySitter(self.billable_start_time)
+    on_times = [
+        billable_start_time, datetime.time(17, 1), datetime.time(17, 29), datetime.time(18, 29)
+    ]
 
-        assert doubtfire.start_time == self.billable_start_time
+    @pytest.mark.parametrize("start", on_times)
+    def test_babysitter_starttime_ontime(self, start):
+        doubtfire = babysitter.BabySitter(start)
+
+        assert doubtfire.start_time == start
 
     early_times = [
         datetime.time(16, 59), datetime.time(16, 29)
