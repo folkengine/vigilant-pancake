f4175ae59c42c09c1c9d84538a5672c6b4088f50
James Prior <jep200404@columbus.rr.com>
2017-12-05 14:16:39 -0500

    babysitter.py: billable_start_time -> earliest_billable_start_time

--- babysitter.py
+++ babysitter.py
@@ -1,8 +1,8 @@
 import datetime
 
 class BabySitter():
-    billable_start_time = datetime.time(17, 0)
+    earliest_billable_start_time = datetime.time(17, 0)
 
     def __init__(self, start_time):
-        self.start_time = max(start_time, self.billable_start_time)
+        self.start_time = max(start_time, self.earliest_billable_start_time)
 
